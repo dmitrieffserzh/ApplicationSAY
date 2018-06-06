ef99e9bd7e429b6b1968a8f701bdd5302b1ca6da
dmitrieffserzh <dmitrieffserzh@yandex.ru>
2018-06-06 18:08:20 +0300

    update

--- resources/views/main/components/users/user_menu_top.blade.php
+++ resources/views/main/components/users/user_menu_top.blade.php
@@ -31,8 +31,6 @@
 
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell"><path d="M22 17H2a3 3 0 0 0 3-3V9a7 7 0 0 1 14 0v5a3 3 0 0 0 3 3zm-8.27 4a2 2 0 0 1-3.46 0"></path></svg>
 
-
-
     <a class="d-inline-block" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <img class="rounded-circle"
              style="width: 40px; height: 40px; border: 2px Solid rgba(255, 255, 255, 0.1);"
